a9b1c3b4c872a9b7ac5b02ae7c44a58d1a2121d8
Otávio/Wumq <128698772+Wumq157@users.noreply.github.com>
2023-05-25 17:15:58 -0300

    a

--- scripts.js
+++ scripts.js
@@ -1,28 +1,37 @@
 // Função para carregar o conteúdo das páginas
 function loadPageContent(pageName) {
-    // Carrega o cabeçalho
-    fetch('head.html')
-      .then(response => response.text())
-      .then(headContent => {
+    // Cria uma requisição para o cabeçalho
+    var headRequest = new XMLHttpRequest();
+    headRequest.open('GET', 'head.html', true);
+    headRequest.onreadystatechange = function () {
+      if (headRequest.readyState === 4 && headRequest.status === 200) {
         // Insere o cabeçalho na página atual
-        document.querySelector('head').innerHTML = headContent;
-      });
+        document.querySelector('head').innerHTML = headRequest.responseText;
+      }
+    };
+    headRequest.send(null);
   
-    // Carrega o rodapé
-    fetch('footer.html')
-      .then(response => response.text())
-      .then(footerContent => {
+    // Cria uma requisição para o rodapé
+    var footerRequest = new XMLHttpRequest();
+    footerRequest.open('GET', 'footer.html', true);
+    footerRequest.onreadystatechange = function () {
+      if (footerRequest.readyState === 4 && footerRequest.status === 200) {
         // Insere o rodapé na página atual
-        document.querySelector('footer').innerHTML = footerContent;
-      });
+        document.querySelector('footer').innerHTML = footerRequest.responseText;
+      }
+    };
+    footerRequest.send(null);
   
-    // Carrega o conteúdo específico da página
-    fetch(pageName)
-      .then(response => response.text())
-      .then(pageContent => {
+    // Cria uma requisição para o conteúdo da página
+    var pageRequest = new XMLHttpRequest();
+    pageRequest.open('GET', pageName, true);
+    pageRequest.onreadystatechange = function () {
+      if (pageRequest.readyState === 4 && pageRequest.status === 200) {
         // Insere o conteúdo na seção principal da página
-        document.querySelector('main').innerHTML = pageContent;
-      });
+        document.querySelector('main').innerHTML = pageRequest.responseText;
+      }
+    };
+    pageRequest.send(null);
   }
   
   // Obtém o nome da página atual
