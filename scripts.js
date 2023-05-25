c3348b8cd3db5ec16557d05eaefbf6043059b7dc
Otávio/Wumq <128698772+Wumq157@users.noreply.github.com>
2023-05-25 17:22:16 -0300

    a

--- scripts.js
+++ scripts.js
@@ -1,42 +1,43 @@
-// Função para carregar o conteúdo das páginas
-function loadPageContent(pageName) {
-    // Cria uma requisição para o cabeçalho
-    var headRequest = new XMLHttpRequest();
-    headRequest.open('GET', 'head.html', true);
-    headRequest.onreadystatechange = function () {
-      if (headRequest.readyState === 4 && headRequest.status === 200) {
-        // Insere o cabeçalho na página atual
-        document.querySelector('head').innerHTML = headRequest.responseText;
-      }
-    };
-    headRequest.send(null);
-  
-    // Cria uma requisição para o rodapé
-    var footerRequest = new XMLHttpRequest();
-    footerRequest.open('GET', 'footer.html', true);
-    footerRequest.onreadystatechange = function () {
-      if (footerRequest.readyState === 4 && footerRequest.status === 200) {
-        // Insere o rodapé na página atual
-        document.querySelector('footer').innerHTML = footerRequest.responseText;
-      }
-    };
-    footerRequest.send(null);
-  
-    // Cria uma requisição para o conteúdo da página
-    var pageRequest = new XMLHttpRequest();
-    pageRequest.open('GET', pageName, true);
-    pageRequest.onreadystatechange = function () {
-      if (pageRequest.readyState === 4 && pageRequest.status === 200) {
-        // Insere o conteúdo na seção principal da página
-        document.querySelector('main').innerHTML = pageRequest.responseText;
-      }
-    };
-    pageRequest.send(null);
+// Função para carregar o conteúdo do cabeçalho
+function loadHeader() {
+    fetch('head.html')
+      .then(response => response.text())
+      .then(headerContent => {
+        const headerElement = document.createElement('header');
+        headerElement.innerHTML = headerContent;
+        document.body.insertBefore(headerElement, document.body.firstChild);
+      });
   }
   
-  // Obtém o nome da página atual
-  const currentPage = window.location.pathname.split('/').pop();
+  // Função para carregar o conteúdo do rodapé
+  function loadFooter() {
+    fetch('footer.html')
+      .then(response => response.text())
+      .then(footerContent => {
+        const footerElement = document.createElement('footer');
+        footerElement.innerHTML = footerContent;
+        document.body.appendChild(footerElement);
+      });
+  }
   
-  // Carrega o conteúdo da página atual
-  loadPageContent(currentPage);
+  // Função para carregar o conteúdo do index.html
+  function loadContent() {
+    fetch('index.html')
+      .then(response => response.text())
+      .then(indexContent => {
+        const mainElement = document.createElement('main');
+        mainElement.innerHTML = indexContent;
+        document.body.appendChild(mainElement);
+      });
+  }
+  
+  // Carregar o cabeçalho, conteúdo e rodapé
+  function loadPage() {
+    loadHeader();
+    loadContent();
+    loadFooter();
+  }
+  
+  // Chamar a função para carregar a página
+  loadPage();
   
