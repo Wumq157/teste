0d28b12ef6d4378c1fd1bb0cfa757468467ab4f7
Otávio/Wumq <128698772+Wumq157@users.noreply.github.com>
2023-05-25 17:31:56 -0300

    sla porra

--- scripts.js
+++ scripts.js
@@ -1,43 +1,13 @@
-// Função para carregar o conteúdo do cabeçalho
-function loadHeader() {
-    fetch('head.html')
-      .then(response => response.text())
-      .then(headerContent => {
-        const headerElement = document.createElement('header');
-        headerElement.innerHTML = headerContent;
-        document.body.insertBefore(headerElement, document.body.firstChild);
-        return headerContent;
-      });
-  }
-  
-  // Função para carregar o conteúdo do rodapé
-  function loadFooter() {
-    fetch('footer.html')
-      .then(response => response.text())
-      .then(footerContent => {
-        const footerElement = document.createElement('footer');
-        footerElement.innerHTML = footerContent;
-        document.body.appendChild(footerElement);
-        return footerContent;
-      });
-  }
-  
-  // Função para carregar o conteúdo do index.html
-  function loadContent() {
+// Função para carregar o conteúdo do index.html e adicionar à tag <main>
+function loadContent() {
     fetch('index.html')
       .then(response => response.text())
       .then(indexContent => {
-        const mainElement = document.createElement('main');
+        const mainElement = document.querySelector('main');
         mainElement.innerHTML = indexContent;
-  
-        // Carregar cabeçalho e rodapé após o carregamento do conteúdo principal
-        Promise.all([loadHeader(), loadFooter()])
-          .then(() => {
-            document.body.appendChild(mainElement);
-          });
       });
   }
   
-  // Chamar a função para carregar a página
+  // Chamar a função para carregar o conteúdo do index.html
   loadContent();
   
